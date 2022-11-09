9d981b0464b8573662088a3c244fab85a45f3ff2
Brian VG <brian.vangrinsven@gmail.com>
2022-11-08 22:19:25 -0500

    fixed mapping and added default usage into dll as well

--- CopySqlBulkDataCmdLet.cs
+++ CopySqlBulkDataCmdLet.cs
@@ -9,160 +9,172 @@ using pscrossdemo.utils;
 
 namespace pscrossdemo
 {
-    [Cmdlet(VerbsCommon.Copy, "SqlBulkData")]
-    [OutputType(typeof(JobStatus))]
-    public class CopySqlBulkDataCmdLet : PSCmdlet
+  [Cmdlet(VerbsCommon.Copy, "SqlBulkData")]
+  [OutputType(typeof(JobStatus))]
+  public class CopySqlBulkDataCmdLet : PSCmdlet
+  {
+    private JobStatus js = new JobStatus();
+    private List<SchemaProp> srcCols;
+    private List<SchemaProp> destCols;
+
+    private ProgressRecord pgRec = new ProgressRecord(0, "Copying Data", "Starting Process");
+
+    [Parameter(Mandatory = true)]
+    public string SrcConnString { get; set; }
+
+    [Parameter(Mandatory = true)]
+    public string DestConnString { get; set; }
+
+    [Parameter(Mandatory = true)]
+    public string SrcTable { get; set; }
+
+    [Parameter(Mandatory = true)]
+    public string DestTable { get; set; }
+
+    [Parameter(Mandatory = false)]
+    public SwitchParameter TruncateDestTable { get; set; }
+
+    [Parameter(Mandatory = false)]
+    public SwitchParameter MapColumns { get; set; }
+
+    [Parameter(Mandatory = false)]
+    public SwitchParameter UseDefault { get; set; }
+
+
+
+    // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+    protected override void BeginProcessing()
     {
-        private JobStatus js = new JobStatus();
-        private SqlConnection srcConn;
-        private SqlConnection dstConn;
+      WriteVerbose("Initializing the transfer objects");
+      js.SourceTable = SrcTable;
+      js.DestinationTable = DestTable;
 
-        private List<SchemaProp> srcCols;
-        private List<SchemaProp> destCols;
+      WriteVerbose("Get the Number of Rows In Source Table");
+      js.SourceRowCount = SqlAccess.GetSQLConnection(SrcConnString).GetScalar<long>(Helpers.GetRowCountSql(SrcTable));
 
-        private ProgressRecord pgRec = new ProgressRecord(0, "Copying Data", "Starting Process");
+      WriteVerbose("Get the Schemas from the source and destination tables");
+      srcCols = Helpers.DataTableSchemaProp(SqlAccess.GetSQLConnection(SrcConnString).GetDataTable(Helpers.GetSchemaFromDBSql(SrcTable)));
+      destCols = Helpers.DataTableSchemaProp(SqlAccess.GetSQLConnection(DestConnString).GetDataTable(Helpers.GetSchemaFromDBSql(DestTable)));
 
-        [Parameter(Mandatory = true)]
-        public string SrcConnString { get; set; }
+      if (TruncateDestTable)
+      {
+        WriteVerbose("Clearing Destination Table Data");
+        SqlAccess.GetSQLConnection(DestConnString).GetNonQuery(Helpers.TruncateTableSql(DestTable));
+        WriteVerbose("Table Cleared");
+      }
 
-        [Parameter(Mandatory = true)]
-        public string DestConnString { get; set; }
-
-        [Parameter(Mandatory = true)]
-        public string SrcTable { get; set; }
-
-        [Parameter(Mandatory = true)]
-        public string DestTable { get; set; }
-
-        [Parameter(Mandatory = false)]
-        public SwitchParameter TruncateDestTable { get; set; }
-
-        [Parameter(Mandatory = false)]
-        public SwitchParameter MapColumns { get; set; }
-
-
-        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
-        protected override void BeginProcessing()
-        {
-            WriteVerbose("Initializing the transfer objects");
-            js.SourceTable = SrcTable;
-            js.DestinationTable = DestTable;
-
-            WriteVerbose("Initilize Connections to SQL Servers");
-            srcConn = new SqlConnection(SrcConnString);
-            dstConn = new SqlConnection(DestConnString);
-
-            WriteVerbose("Get the Number of Rows In Source Table");
-            js.SourceRowCount = srcConn.GetScalar<long>(Helpers.GetRowCountSql(SrcTable));
-
-            WriteVerbose("Get the Schemas from the source and destination tables");
-            srcCols = Helpers.DataTableSchemaProp(srcConn.GetDataTable(Helpers.GetSchemaFromDBSql(SrcTable)));
-            destCols = Helpers.DataTableSchemaProp(dstConn.GetDataTable(Helpers.GetSchemaFromDBSql(DestTable)));
-
-            if (TruncateDestTable)
-            {
-                WriteVerbose("Clearing Destination Table Data");
-                dstConn.GetNonQuery(Helpers.TruncateTableSql(DestTable));
-                WriteVerbose("Table Cleared");
-            }
-
-            js.SourcePullSql = Helpers.GetPullAllSql(SrcTable, srcCols, destCols);
-
-        }
-
-        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
-        protected override void ProcessRecord()
-        {
-
-            WriteVerbose("Ran the Datareader");
-            var reader = srcConn.GetDataReader(js.SourcePullSql);
-
-            using (SqlConnection destConn = dstConn)
-            {
-                destConn.Open();
-                var transaction = destConn.BeginTransaction();
-                WriteVerbose("Started the Transaction");
-
-                using (var sqlBulk = new SqlBulkCopy(destConn, SqlBulkCopyOptions.KeepIdentity, transaction))
-                {
-                    sqlBulk.SqlRowsCopied += OnReportSqlRowsCopied;
-                    sqlBulk.BulkCopyTimeout = 3600;
-                    sqlBulk.NotifyAfter = (int)((double)js.SourceRowCount * 0.1);
-                    sqlBulk.BatchSize = (int)((double)js.SourceRowCount * 0.001);
-
-                    WriteVerbose("Defining the BulkCopyObject");
-                    sqlBulk.DestinationTableName = string.Format("[{0}]", DestTable);
-
-                    if (MapColumns)
-                    {
-                        CreateColumnMapping(sqlBulk.ColumnMappings);
-                    }
-
-                    WriteVerbose("Starting the Data Transfer Process");
-                    sqlBulk.WriteToServer(reader);
-                }
-
-                WriteVerbose("Completed Writing to the server");
-
-                transaction.Commit();
-                WriteVerbose("Transaction Complete");
-
-                js.DestinationRowCount = dstConn.GetScalar<long>(Helpers.GetRowCountSql(DestTable));
-            }
-            reader.Close();
-
-        }
-
-        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
-        protected override void EndProcessing()
-        {
-            WriteVerbose("Complete");
-            WriteObject(js);
-        }
-
-
-        #region "Events"
-        private void OnReportSqlRowsCopied(object sender, SqlRowsCopiedEventArgs e)
-        {
-            pgRec.StatusDescription = string.Format("Row Count: {0} / {1}", e.RowsCopied, js.SourceRowCount);
-
-            pgRec.PercentComplete = (int)(e.RowsCopied * 100 / js.SourceRowCount);
-
-            WriteProgress(pgRec);
-        }
-
-        #endregion
-
-        #region "Helper Functions"
-
-        private void CreateColumnMapping(SqlBulkCopyColumnMappingCollection sqlBulkCopyColumnMappingCollection)
-        {
-            foreach (SchemaProp dstCol in destCols)
-            {
-                if (srcCols.FindIndex(x => x.ColumnName == dstCol.ColumnName) > 0)
-                {
-                    sqlBulkCopyColumnMappingCollection.Add(dstCol.ColumnName, dstCol.ColumnName);
-                }
-            }
-        }
-
-        #endregion
+      js.SourcePullSql = Helpers.GetPullAllSql(SrcTable, srcCols, destCols, UseDefault);
 
     }
 
-    public class JobStatus
+    // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+    protected override void ProcessRecord()
     {
-        public int Result { get; set; } = 0;
 
-        public string SourceTable { get; set; } = string.Empty;
+      using (SqlConnection destConn = SqlAccess.GetSQLConnection(DestConnString))
+      {
+        destConn.Open();
+        var transaction = destConn.BeginTransaction();
+        WriteVerbose("Started the Transaction");
 
-        public long SourceRowCount { get; set; } = 0;
+        try
+        {
+          WriteVerbose("Ran the Datareader");
+          var reader = SqlAccess.GetSQLConnection(SrcConnString).GetDataReader(js.SourcePullSql);
 
-        public string DestinationTable { get; set; } = string.Empty;
+          using (var sqlBulk = new SqlBulkCopy(destConn, SqlBulkCopyOptions.KeepIdentity, transaction))
+          {
+            sqlBulk.SqlRowsCopied += OnReportSqlRowsCopied;
+            sqlBulk.BulkCopyTimeout = 3600;
+            sqlBulk.NotifyAfter = (int)((double)js.SourceRowCount * 0.05);
+            sqlBulk.BatchSize = (int)((double)js.SourceRowCount * 0.001);
 
-        public long DestinationRowCount { get; set; } = 0;
+            WriteVerbose("Defining the BulkCopyObject");
+            sqlBulk.DestinationTableName = string.Format("[{0}]", DestTable);
 
-        public string SourcePullSql { get; set; } = string.Empty;
+            if (MapColumns)
+            {
+              CreateColumnMapping(sqlBulk.ColumnMappings);
+            }
+
+            WriteVerbose("Starting the Data Transfer Process");
+            sqlBulk.WriteToServer(reader);
+          }
+
+          WriteVerbose("Completed Writing to the server");
+
+          reader.Close();
+          transaction.Commit();
+          WriteVerbose("Transaction Complete");
+
+          js.DestinationRowCount = SqlAccess.GetSQLConnection(DestConnString).GetScalar<long>(Helpers.GetRowCountSql(DestTable));
+
+        }
+        catch (System.Exception ex)
+        {
+          WriteError(new ErrorRecord(
+                   ex,
+                   ex.Message,
+                   ErrorCategory.InvalidResult,
+                   js));
+
+        }
+      }
 
     }
+
+    // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+    protected override void EndProcessing()
+    {
+      WriteVerbose("Complete");
+      WriteObject(js);
+    }
+
+
+    #region "Events"
+    private void OnReportSqlRowsCopied(object sender, SqlRowsCopiedEventArgs e)
+    {
+      pgRec.PercentComplete = (int)(e.RowsCopied * 100 / js.SourceRowCount);
+
+      pgRec.StatusDescription = string.Format("Row Count: {0} / {1} -- {2}%", e.RowsCopied, js.SourceRowCount, pgRec.PercentComplete);
+
+      WriteVerbose(pgRec.StatusDescription);
+
+      WriteProgress(pgRec);
+    }
+
+    #endregion
+
+    #region "Helper Functions"
+
+    private void CreateColumnMapping(SqlBulkCopyColumnMappingCollection sqlBulkCopyColumnMappingCollection)
+    {
+      foreach (SchemaProp dstCol in destCols)
+      {
+        if (srcCols.Exists(x => x.ColumnName == dstCol.ColumnName))
+        {
+          sqlBulkCopyColumnMappingCollection.Add(dstCol.ColumnName, dstCol.ColumnName);
+        }
+      }
+    }
+
+    #endregion
+
+  }
+
+  public class JobStatus
+  {
+    public int Result { get; set; } = 0;
+
+    public string SourceTable { get; set; } = string.Empty;
+
+    public long SourceRowCount { get; set; } = 0;
+
+    public string DestinationTable { get; set; } = string.Empty;
+
+    public long DestinationRowCount { get; set; } = 0;
+
+    public string SourcePullSql { get; set; } = string.Empty;
+
+  }
 }
--- utils/Helpers.cs
+++ utils/Helpers.cs
@@ -5,96 +5,98 @@ using System.Text;
 
 namespace pscrossdemo.utils
 {
-    public static class Helpers
+  public static class Helpers
+  {
+
+    public static string GetSchemaFromDBSql(string tableName)
     {
 
-        public static string GetSchemaFromDBSql(string tableName)
-        {
-
-            return string.Format("SELECT COLUMN_NAME as ColumnName, ORDINAL_POSITION as OrdinalPosition, DATA_TYPE as DataType " +
-            "FROM INFORMATION_SCHEMA.COLUMNS " +
-            "WHERE TABLE_NAME = '{0}' " +
-            "ORDER BY 2", tableName);
-        }
-
-        public static string GetRowCountSql(string tableName)
-        {
-            return string
-                    .Format("SELECT CAST(p.rows AS float) " +
-                    "FROM sys.tables AS tbl " +
-                    "INNER JOIN sys.indexes AS idx ON idx.object_id = tbl.object_id and idx.index_id < 2 " +
-                    "INNER JOIN sys.partitions AS p ON p.object_id=CAST(tbl.object_id AS int) AND p.index_id=idx.index_id " +
-                    "WHERE ((tbl.name='{0}' AND SCHEMA_NAME(tbl.schema_id)='dbo'))",
-                    tableName);
-        }
-
-        public static string TruncateTableSql(string tableName)
-        {
-            return string.Format("TRUNCATE TABLE [{0}}]", tableName);
-        }
-
-
-        public static List<SchemaProp> DataTableSchemaProp(DataTable dtConvert)
-        {
-            List<SchemaProp> result = new List<SchemaProp>();
-
-            foreach (DataRow dr in dtConvert.Rows)
-            {
-                result.Add(SchemaProp.CreateObject(dr));
-            }
-
-            return result;
-        }
-
-        public static string GetPullAllSql(string SrcTableName, List<SchemaProp> srcProps, List<SchemaProp> destProps)
-        {
-            StringBuilder result = new StringBuilder();
-
-            result.AppendLine("SELECT ");
-
-            foreach (SchemaProp dstCol in destProps)
-            {
-                if (srcProps.FindIndex(x => x.ColumnName == dstCol.ColumnName) > 0)
-                {
-                    result.AppendFormat("[{0}], ", dstCol.ColumnName);
-                }
-                else
-                {
-                    switch (dstCol.DataType)
-                    {
-
-                        case "bigint":
-                        case "numeric":
-                        case "bit":
-                        case "smallint":
-                        case "decimal":
-                        case "smallmoney":
-                        case "int":
-                        case "tinyint":
-                        case "money":
-                        case "float":
-                        case "real":
-                            result.AppendFormat("0 as [{0}], ", dstCol.ColumnName);
-                            break;
-
-                        case "char":
-                        case "varchar":
-                        case "text":
-                        case "nchar":
-                        case "nvarchar":
-                        case "ntext":
-                        default:
-                            result.AppendFormat("'' as [{0}], ", dstCol.ColumnName);
-                            break;
-                    }
-
-                }
-            }
-
-            result.AppendFormat("FROM [{0}] \r\n", SrcTableName);
-
-            return result.ToString().Replace(", FROM", " FROM");
-        }
-
+      return string.Format("SELECT COLUMN_NAME as ColumnName, ORDINAL_POSITION as OrdinalPosition, DATA_TYPE as DataType " +
+      "FROM INFORMATION_SCHEMA.COLUMNS " +
+      "WHERE TABLE_NAME = '{0}' " +
+      "ORDER BY 2", tableName);
     }
+
+    public static string GetRowCountSql(string tableName)
+    {
+      return string
+              .Format("SELECT CAST(p.rows AS float) " +
+              "FROM sys.tables AS tbl " +
+              "INNER JOIN sys.indexes AS idx ON idx.object_id = tbl.object_id and idx.index_id < 2 " +
+              "INNER JOIN sys.partitions AS p ON p.object_id=CAST(tbl.object_id AS int) AND p.index_id=idx.index_id " +
+              "WHERE ((tbl.name='{0}' AND SCHEMA_NAME(tbl.schema_id)='dbo'))",
+              tableName);
+    }
+
+    public static string TruncateTableSql(string tableName)
+    {
+      return string.Format("TRUNCATE TABLE [{0}]", tableName);
+    }
+
+
+    public static List<SchemaProp> DataTableSchemaProp(DataTable dtConvert)
+    {
+      List<SchemaProp> result = new List<SchemaProp>();
+
+      foreach (DataRow dr in dtConvert.Rows)
+      {
+        result.Add(SchemaProp.CreateObject(dr));
+      }
+
+      return result;
+    }
+
+    public static string GetPullAllSql(string SrcTableName, List<SchemaProp> srcProps, List<SchemaProp> destProps, bool UseDefault = false)
+    {
+      StringBuilder result = new StringBuilder();
+
+      result.Append("SELECT ");
+
+      foreach (SchemaProp dstCol in destProps)
+      {
+        if (srcProps.Exists(x => x.ColumnName == dstCol.ColumnName))
+        {
+          result.AppendFormat("[{0}], ", dstCol.ColumnName);
+        }
+        else
+        {
+          if (UseDefault)
+          {
+            switch (dstCol.DataType)
+            {
+
+              case "bigint":
+              case "numeric":
+              case "bit":
+              case "smallint":
+              case "decimal":
+              case "smallmoney":
+              case "int":
+              case "tinyint":
+              case "money":
+              case "float":
+              case "real":
+                result.AppendFormat("0 as [{0}], ", dstCol.ColumnName);
+                break;
+
+              case "char":
+              case "varchar":
+              case "text":
+              case "nchar":
+              case "nvarchar":
+              case "ntext":
+              default:
+                result.AppendFormat("'' as [{0}], ", dstCol.ColumnName);
+                break;
+            }
+          }
+        }
+      }
+
+      result.AppendFormat("FROM [{0}] \r\n", SrcTableName);
+
+      return result.ToString().Replace(", FROM", " FROM");
+    }
+
+  }
 }
