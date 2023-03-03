608e206691ea8696c60c599c8e23f5efa3bdcda3
Brian Van Grinsven <brian.vangrinsven@gmail.com>
2023-03-02 19:28:37 -0500

    updates

--- manifestbuild.ps1
+++ manifestbuild.ps1
@@ -1,6 +1,6 @@
 $manifestSplat = @{
   Path              = ".\pscrossdemo.psd1"
-  Author            = 'Kevin Marquette'
+  Author            = 'BVG'
   NestedModules     = @('bin\Debug\netstandard2.0\pscrossdemo.dll')
   RootModule        = "$module.psm1"
   FunctionsToExport = @('Copy-SqlBulkData', 'Test-ProgressBarUsage', 'Test-SampleCmdlet')
